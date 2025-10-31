38f73a20427199b9790c8de0ad33f3a7f062110f
Vasquez86 <vasvoja@gmail.com>
2025-10-31 16:43:06 +0100

    Add 16-bit PCM playback support

--- micropython/main.py
+++ micropython/main.py
@@ -9,11 +9,19 @@ from audiopwm import AudioPWM
 PLAYER_PIN = 2
 PWM_BASE_FREQ = 20_000
 SAMPLE_RATE = 8_000
+SAMPLE_BITS = 16
+SIGNED_SAMPLES = True
 CLIP_PATH = "/testera.raw"
 
 
 def main() -> None:
-    player = AudioPWM(pin=PLAYER_PIN, pwm_base_freq=PWM_BASE_FREQ, sample_rate=SAMPLE_RATE)
+    player = AudioPWM(
+        pin=PLAYER_PIN,
+        pwm_base_freq=PWM_BASE_FREQ,
+        sample_rate=SAMPLE_RATE,
+        sample_bits=SAMPLE_BITS,
+        signed=SIGNED_SAMPLES,
+    )
     player.set_volume(0.9)
 
     if not player.play_file(CLIP_PATH):
